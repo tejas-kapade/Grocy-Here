831b4cf68d362238ba1252725796e7c074239ae8
Tejas Kapade <75604489+tejas-kapade@users.noreply.github.com>
2023-10-05 20:34:57 +0530

    Add files via upload

---
+++ GROCYHERE.sql
@@ -0,0 +1,128 @@
+-- MariaDB dump 10.19  Distrib 10.5.12-MariaDB, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: GROCERY
+-- ------------------------------------------------------
+-- Server version	10.5.12-MariaDB-1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `entries`
+--
+
+DROP TABLE IF EXISTS `entries`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `entries` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `date` varchar(255) DEFAULT NULL,
+  `time` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=32 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `entries`
+--
+
+LOCK TABLES `entries` WRITE;
+/*!40000 ALTER TABLE `entries` DISABLE KEYS */;
+INSERT INTO `entries` VALUES (1,'Tejas','3434','09-03-2023','04:05:36 pm'),(2,'Priya','priyak90','09-03-2023','04:08:56 pm'),(3,'Alita','hugo','09-03-2023','04:17:07 pm'),(4,'Tejas','3434','09-03-2023','04:20:52 pm'),(5,'Priya','priyak90','09-03-2023','04:22:10 pm'),(6,'Tejas','3434','09-03-2023','04:23:18 pm'),(7,'Tejas','3434','09-03-2023','11:59:55 pm'),(8,'Mayur','mm90','10-03-2023','12:03:54 am'),(9,'Alita','hugo','10-03-2023','12:07:34 am'),(10,'Priya','priyak90','10-03-2023','03:34:27 pm'),(11,'Alita','hugo','10-03-2023','04:00:50 pm'),(12,'Tejas','3434','12-03-2023','02:52:00 pm'),(13,'Alita','hugo','21-03-2023','03:10:54 pm'),(14,'Alita','hugo','21-03-2023','03:14:27 pm'),(15,'Tejas','3434','21-03-2023','03:23:26 pm'),(16,'Alita','hugo','21-03-2023','03:47:56 pm'),(17,'Alita','hugo','21-03-2023','03:52:01 pm'),(18,'Tejas','3434','21-03-2023','03:56:19 pm'),(19,'Tejas','3434','21-03-2023','07:01:33 pm'),(20,'Alita','hugo','21-03-2023','07:04:01 pm'),(21,'Tejas','3434','26-03-2023','01:58:33 pm'),(22,'niraj','nijju','29-03-2023','12:05:13 pm'),(23,'Tejas','3434','29-03-2023','12:06:29 pm'),(24,'tejas','3434','29-03-2023','12:17:46 pm'),(25,'Tejas','3434','29-03-2023','12:24:29 pm'),(26,'Niraj','nijju','29-03-2023','12:40:40 pm'),(27,'tejas','3434','25-04-2023','10:53:18 am'),(28,'Tejas','3434','25-04-2023','01:20:17 pm'),(29,'Ambuja','12345','04-10-2023','09:32:48 pm'),(30,'Ambuja','12345','04-10-2023','11:00:26 pm'),(31,'Ambuja','12345','05-10-2023','01:43:33 pm');
+/*!40000 ALTER TABLE `entries` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `products`
+--
+
+DROP TABLE IF EXISTS `products`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `products` (
+  `name` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `products`
+--
+
+LOCK TABLES `products` WRITE;
+/*!40000 ALTER TABLE `products` DISABLE KEYS */;
+INSERT INTO `products` VALUES ('Tejas');
+/*!40000 ALTER TABLE `products` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `register`
+--
+
+DROP TABLE IF EXISTS `register`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `register` (
+  `NAME` varchar(255) DEFAULT NULL,
+  `EMAIL` varchar(255) DEFAULT NULL,
+  `PHONE` varchar(255) DEFAULT NULL,
+  `ADDRESS` varchar(255) DEFAULT NULL,
+  `PINCODE` varchar(255) DEFAULT NULL,
+  `PASSWORD` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `register`
+--
+
+LOCK TABLES `register` WRITE;
+/*!40000 ALTER TABLE `register` DISABLE KEYS */;
+INSERT INTO `register` VALUES ('Tejas','tejaskapade@gmail.com','9892624891','HN.Mohone, Ambivali','421102','3434'),('Nano','nainishcoc@gmail.com','9822739121','Krishna Plaza, Mohone','421101','NANOCOC'),('Raadha','raadakrishna@gmail.com','90090099','Vrindaavan','14300','krishna'),('Alita','battleangel@gmail.com','8765489898','Iron city, street 365','3434','hugo'),('admin','admin@gmail.com','000000000','ADMIN','ADMIN','9090'),('Aniket','aniketpunase@gmail.com','8989898989','Muktainagar, Jalgaon','422333','punase'),('Priya','priyakapade@gmail.com','8657483959','HN, Panchashil Nagar, Mohone','421102','priyak90'),('Mayur','mayurmalkhede@gmail.com','83380999','Galegaon,Mohone','431123','mm90'),('Krishna','devkinandan@gmail.com','90909090','Golok dhaam, Vrindaavan','421124','raadha'),('Jayesh','hivarkar@gmail.com','998223111','Savantwaadi R1 road vapi, Gujraat','431111','golu'),('Niraj','niluu@gmail.com','9999999','Kongaon,kalyan','9000','nijju'),('Ambuja','ambujacement@gmail.com','9876543210','Bhoot Galli, Shamshaan ghat- Varanasi','987000','12345');
+/*!40000 ALTER TABLE `register` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `transaction`
+--
+
+DROP TABLE IF EXISTS `transaction`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `transaction` (
+  `name` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `items` varchar(255) DEFAULT NULL,
+  `paid` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `transaction`
+--
+
+LOCK TABLES `transaction` WRITE;
+/*!40000 ALTER TABLE `transaction` DISABLE KEYS */;
+INSERT INTO `transaction` VALUES ('Priya','priyakapade@gmail.com','2','116.98'),('Priya','priyakapade@gmail.com','2','75.58'),('Mayur','mayurmalkhede@gmail.com','2','269.10'),('Tejas','tejaskapade@gmail.com','9','1124.10'),('Tejas','tejaskapade@gmail.com','4','176.40'),('Alita','battleangel@gmail.com','6','372.60'),('Tejas','tejaskapade@gmail.com','6','621.00'),('niraj','niluu@gmail.com','2','180.00'),('Tejas','tejaskapade@gmail.com','3','349.20'),('tejas','tejaskapade@gmail.com','3','142.20'),('Tejas','tejaskapade@gmail.com','3','98.10'),('tejas','tejaskapade@gmail.com','3','331.20'),('Tejas','tejaskapade@gmail.com','4','1391.40'),('Ambuja','ambujacement@gmail.com','5','612.90'),('Ambuja','ambujacement@gmail.com','1','225.00');
+/*!40000 ALTER TABLE `transaction` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-10-05 13:57:32
